5c50cd283f6667022aae7e531a71a88433313b47
cami <packlou69@gmail.com>
2026-08-29 13:59:56 -0400

    utilisation d'une variable d'environnement

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-Cami = input("comment t'appelles-tu ? ")
-print("Bonjour " + Cami + " !")
-print("Tu viens de lancer ton premier programme Python.")
-print("Je commence la DevSecOps !")
+import os
+
+Cami = input("Comment t'appelles-tu ? ")
+message = os.getenv("MESSAGE_SECRET" , "Bonjour")
+print(message + " " + Cami + " !") 
+
 
