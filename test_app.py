b8c4380a06bcaadc2588ed0551fd196d433a35ef
cami <packlou69@gmail.com>
2026-09-11 11:54:46 -0400

    feat: nettoyer les espaces du nom

--- test_app.py
+++ test_app.py
@@ -4,3 +4,5 @@ from app import create_message
 def test_create_message():
     assert create_message("Cami") == "Bonjour Cami !"
 
+def test_create_message_supprime_les_espaces():
+    assert create_message(" Cami ") == "Bonjour Cami !"
